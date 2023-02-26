748c22cfe11d2be9652a50dfa011eaf3ae3a682f
LHchien33 <judy263562@gmail.com>
2023-02-26 11:32:34 +0800

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/test_actions/',
+  // base: '/test_actions/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
